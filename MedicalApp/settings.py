2375cd2f39c320e73ca5b52b8ab8b7dfcf978e02
rahul <rahul@syonlab.com>
2017-05-06 21:43:21 +0530

    fourth push

--- MedicalApp/settings.py
+++ MedicalApp/settings.py
@@ -11,13 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import sys
-
-path = '/home/<your-PythonAnywhere-username>/my-first-blog'  # use your own PythonAnywhere username here
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
