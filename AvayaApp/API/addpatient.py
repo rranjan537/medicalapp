d71f5104c813241dfbcc20e6a8a58cd5fbff15cc
rahul <rahul@syonlab.com>
2017-05-06 21:31:46 +0530

    Thid push

--- AvayaApp/API/addpatient.py
+++ AvayaApp/API/addpatient.py
@@ -2,9 +2,10 @@ from AvayaApp.models import Patient
 
 def addData(self, request, format = None):
 	ob = Patient()
-	print request['Name']
-	ob.Name = request['Name']
+	# print request['Name']
+
 	ob.Age = request['Age']
+	ob.Name = request['Name']
 	if(request['Age']<'18'):
 		ob.AgeGroup = '1'
 	elif(request['Age']>='18' and request['Age']<'40'):
@@ -14,5 +15,7 @@ def addData(self, request, format = None):
 	elif(request['Age']>='60'):
 		ob.AgeGroup = '4'
 	ob.UID = request['UID']
+	ob.UserName = request['UserName']
+	ob.Password = request['Password']
 	ob.save()
 	return('200')
--- MedicalApp/settings.py
+++ MedicalApp/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
+
+path = '/home/<your-PythonAnywhere-username>/my-first-blog'  # use your own PythonAnywhere username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
